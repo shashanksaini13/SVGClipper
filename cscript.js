c343459d256bda4bc2a2977958b77cc661b5921d
shashanksaini13 <sainishashank@aol.com>
2022-09-19 21:26:54 -0400

    Update cscript.js

--- cscript.js
+++ cscript.js
@@ -1,12 +1,18 @@
 //let svgs = Array.from(document.querySelectorAll('img[src*=".svg"]'));
-//console.log(svgs)
 let svgs = document.querySelectorAll('svg')
+let i = 0
+Array.from(svgs, e => {
+  let first = e.getBoundingClientRect()
+  console.log("SVG #" + i + "\nX: " + first.x + "\nY: " + first.y + "\nWidth: " + first.width + "\nHeight: " + first.height)
+  i++
+}
+);
+console.log(svgs)
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.message === "test") {
         //console.log(Array.from(svgs)[0].outerHTML)
         //sendResponse({resp: Array.from(svgs)})
-        console.log(svgs.item(0).outerHTML)
         sendResponse({resp: svgs.item(0).outerHTML});
       }
     }
